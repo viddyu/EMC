982e290259b3e6fb0f975277d8534fb26e856aec
Brandon Triplett <btriplett216757@gmail.com>
2018-06-23 13:15:44 -0400

    Modified folder structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 import Forms from "./components/forms/Forms.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1 className="App-title">Emergency Medical Communication</h1>
         </header>
         <br/>
         <br/>
