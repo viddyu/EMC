3025f5de7eeb568d7ea5833adeb2fd4f4165480b
kerrickwoyshner <kerrick.woyshner@gmail.com>
2018-06-27 12:45:58 -0400

    added form settings

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,47 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 import "./Form.css";
 
 class Form extends Component {
     // Setting the component's initial state
     state = {
+        vitals: [],
         firstName: "",
         lastName: "",
         age: "",
         bloodType: "",
         heartRate: "",
         breathRate: "",
-        bloodPressure: ""
+        bloodPressure: "",
+        notes: ""
+    };
+
+    loadVitals = () => {
+        API.getVitals()
+            .then(res =>
+                this.setState({
+                    vitals: res.data,
+                    firstName: "",
+                    lastName: "",
+                    age: "",
+                    bloodType: "",
+                    heartRate: "",
+                    breathRate: "",
+                    bloodPressure: "",
+                    notes: ""
+                })
+            )
+            .catch(err => console.log(err));
+    };
+
+    // componentDidMount() {
+    //     this.loadVitals();
+    // }
+
+    deleteVital = id => {
+        API.deleteVital(id)
+            .then(res => this.loadVitals())
+            .catch(err => console.log(err));
     };
 
     handleInputChange = event => {
@@ -27,6 +58,37 @@ class Form extends Component {
         // Preventing the default behavior of the form submit 
         event.preventDefault();
 
+        if (this.state.firstName &&
+            this.state.lastName &&
+            this.state.bloodPressure &&
+            this.state.breathRate &&
+            this.state.heartRate) {
+
+            alert(`Submitted:
+            First name: ${this.state.firstName}\n 
+            Last name: ${this.state.lastName}\n
+            Age: ${this.state.age}\n
+            Blood Type: ${this.state.bloodType}\n
+            Heart Rate: ${this.state.heartRate}\n
+            Breath Rate: ${this.state.breathRate}\n
+            Blood Pressure: ${this.state.bloodPressure}\n
+            Notes: ${this.state.notes}`);
+
+            // API.saveVital({
+            //     firstName: this.state.firstName,
+            //     lastName: this.state.lastName,
+            //     age: this.state.age,
+            //     bloodType: this.state.bloodType,
+            //     heartRate: this.state.heartRate,
+            //     breathRate: this.state.breathRate,
+            //     bloodPressure: this.state.bloodPressure,
+            // })
+            // .then(res => this.loadVitals())
+            // .catch(err => console.log(err));
+        } else {
+            alert('You left one or more vital entries blank. \nPlease provide all of the vitals information.')
+        }
+
         // Alert the user their first and last name, clear 'this.state.firstName' and 'this.state.lastName', 
         // clearing the inputs
         console.log(this.state);
@@ -38,7 +100,8 @@ class Form extends Component {
             bloodType: "",
             heartRate: "",
             breathRate: "",
-            bloodPressure: ""
+            bloodPressure: "",
+            notes: ""
         });
     };
 
@@ -60,6 +123,8 @@ class Form extends Component {
                     Breath Rate {this.state.breathRate}
                     <br></br>
                     Blood Pressure {this.state.bloodPressure}
+                    <br></br>
+                    Notes {this.state.notes}
                 </p>
                 <form className="form">
                     <input
@@ -83,7 +148,7 @@ class Form extends Component {
                         type="text"
                         placeholder="Age"
                     />
-                     <input
+                    <input
                         value={this.state.gender}
                         name="gender"
                         onChange={this.handleInputChange}
@@ -118,11 +183,19 @@ class Form extends Component {
                         type="text"
                         placeholder="Blood Pressure"
                     />
-                    <button onClick={this.handleFormSubmit}>Submit</button>
+                    <textarea
+                        value={this.state.notes}
+                        onChange={this.handleInputChange}
+                        name="notes"
+                        placeholder="Notes (Optional)"
+                    />
+                    <button
+                        disabled={!(this.state.firstName)}
+                        onClick={this.handleFormSubmit}>Submit</button>
                 </form>
             </div>
         );
     }
 }
 
-export default Form; 
+export default Form;
