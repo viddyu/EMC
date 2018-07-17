55a388a418209a72b1f46321ac7a424cc28eeb46
kerrickwoyshner <kerrick.woyshner@gmail.com>
2018-07-16 20:13:59 -0400

    Directions.js update & replacated the old version @ Directions-old.js

---
+++ client/src/components/Map/Map.js
@@ -0,0 +1,31 @@
+// https://www.youtube.com/watch?v=N1J7Q1qJPQM
+// annotating before 54:10
+import React, { Component } from 'react';
+import { compose, withProps } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL:
+            "https://maps.googleapis.com/maps/api/js?libraries=visualization&key=AIzaSyCdZSZkRVx4Ns7NwSGs6xiKqCEZaDtwwdA",
+        loadingElement: <div style={{ height: `100%`, width: '100%' }} />,
+        containerElement: <div style={{ height: `400px`, width: '400' }} />,
+        mapElement: <div style={{ height: `100%`, width: '100%' }} />
+    }),
+    withScriptjs,
+    withGoogleMap
+)(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 41.5074066, lng: -81.60832649999999 }}>
+        <Marker position={{ lat: 41.5074066, lng: -81.60832649999999 }} />
+    </GoogleMap>
+));
+
+class Map extends Component {
+
+
+    render() {
+        return <MyMapComponent key="map" />
+    }
+}
+
+export default Map;
--- client/src/pages/Directions/Directions.js
+++ client/src/pages/Directions/Directions.js
@@ -1,228 +1,83 @@
 import React, { Component } from "react";
+import Map from "../../components/Map";
+import Places from "../../components/Places";
 import "./Directions.css";
-
-// var map;
-// var marker;
-// var infowindow;
-// var marker_created = [];
-// var mapDiv = document.getElementById('map');
-// var myLatlng = new google.maps.Latlng(1, 1)
-// var directionsService = new google.maps.DirectionsService;
-// var directionsDisplay = new google.maps.DirectionsRenderer;
-// var directionDistance = new google.maps.DistanceMatrixService;
-
-// function initMap() {
-//     map = new google.maps.Map(mapDiv, {
-//         center: myLatlng,
-//         zoom: 13,
-//         zoomControl: false,
-//         streetViewControl: false,
-//         scrollwheel: true
-//     });
-
-//     marker = new google.maps.Marker({
-//         position: myLatlng,
-//         map: map,
-//         title: 'Hello World!',
-//         icon: 'static/images/icon.png',
-//         draggable: true
-//     });
-
-//     var contentString = '<div id="container>' +
-//         '<h1>test infowindow</h1>' +
-//         '</div>';
-
-//     infowindow = new google.maps.Infowindow({
-//         content: contentString,
-//         maxwidth: 300
-//     });
-
-//     marker.addListener('click', function () {
-//         infowindow.open(map, marker);
-//     });
-
-//     map.addListener('click', function () {
-//         infowindow.close();
-//     });
-
-//     google.maps.event.addDomListener(marker, 'dragstart', function (event) {
-//         // console.log('first marker', event);
-//     });
-
-//     google.maps.event.addDomListener(marker, 'dragend', function (event) {
-//         // console.log('second marker', event.latLng.lat(), event.latLng.lng());
-//         getNewMarker();
-//         map.setCenter(marker.getPosition());
-//         map.setZoom(15);
-//     });
-
-//     var customMapType = new google.maps.StyledMapType([
-//         { stylers: [{ hue: '#D2E4C8' }] },
-//         {
-//             featureType: 'water',
-//             stylers: [{ color: '#599459' }]
-//         }
-//     ]);
-
-//     var customMapTypeId = 'custom_style';
-//     map.mapTypes.set(customMapTypeId, customMapType);
-//     map.setMapTypeId(customMapTypeId);
-//     geolocate();
-// };
-
-// // zoom control
-// function ZoomControl() {
-//     var zoomInButton = document.getElementById('zoom-in');
-//     var zoomOutButton = document.getElementById('zoom-out');
-//     google.maps.event.addDomListener(zoomInButton, 'click', function () {
-//         map.setZoom(map.getZoom() + 1);
-//     });
-//     google.maps.event.addDomListener(zoomOutButton, 'click', function () {
-//         map.setZoom(map.getZoom() - 1);
-//     });
-// };
-
-// // get current location when clicked
-// function GeolocationControl() {
-//     var geoButton = document.getElementById('current-location');
-//     google.maps.event.addDomListener(geoButton, 'click', geolocate);
-// };
-
-// // get current location
-// function geolocate() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(function (position) {
-//             // console.log(position.coords.latitude);
-//             var pos = {
-//                 lat: position.coords.latitude,
-//                 lng: position.coords.longitude
-//             };
-
-//             map.setCenter(pos);
-//             marker.setPosition(pos);
-//         });
-//     }
-
-//     else {
-//         alert('Message about the location');
-//     };
-//     // end geolocate
-// }
-
-// // marker list
-// var markers = [
-//     { 'coord': { 'lat': 41.50985499999999, 'lng': -81.606298 }, 'title': 'title information Infowindow 1' },
-//     { 'coord': { 'lat': 41.5065812, 'lng': -81.6049621 }, 'title': 'title information Infowindow 1' },
-//     { 'coord': { 'lat': 41.5034221, 'lng': -81.6212424 }, 'title': 'title information Infowindow 1' },
-// ];
-
-
-
-// function getNewMarker() {
-//     for (var i = markers.length - 1; i >= 0; i--) {
-//         var item = createMarkers(markers[i]);
-//         marker_created.push(item);
-//         // console.log(marker_created);
-//     }
-// };
-
-// function createMarkers(pos) {
-
-//     var newMarker = new google.maps.Marker({
-//         position: pos.coord,
-//         map: map
-//     });
-
-//     var contentString2 =
-
-//         '<div id ="container-infobox">' +
-//         '<h1> <a href="#" class="direction">' + pos.title + '</a><h1>' +
-//         '<div style=" background: url(' + "static/images/picture.jp" + ') no-repeat center; width: 250px; height: 200px"' +
-//         '</div>' +
-//         '</div>';
-
-//     var options = {
-//         content: contentString2,
-//         disableAutoPan: false,
-//         maxWidth: 0,
-//         pixelOffset: new google.maps.Size(-160, -382),
-//         zIndex: null,
-//         boxStyle: {
-//             opacity: 0.75,
-//             width: "280px"
-//         },
-//         closeBoxMargin: "10px 2px 2px 2px",
-//         closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
-//         infoBoxClearance: new google.maps.Size(1, 1),
-//         isHidden: false,
-//         pane: "floatPane",
-//         enableEventPropagation: false
-//     };
-
-//     var ib = new InfoBox(options);
-
-//     ib.addListener('domready', function () {
-//         $(".direction").on('click', function () {
-//             calculateAndDisplayRoute(newMarker);
-//             $('.infobox').fadeOut('300');
-//         });
-//     })
-
-//     newMarker.addListener('click', function () {
-//         $('.infobox').fadeOut('300');
-//         ib.open(map, newMarker);
-//     });
-//     return newMarker;
-// };
-
-// function calculateAndDisplayRoute(newMarker) {
-//     directionsDisplay.setMap(map);
-//     directionsService.route({
-//         origin: marker.getPosition(),
-//         destination: newMarker.getPosition(),
-//         travelMode: google.maps.TravelMode.DRIVING
-//     }, function (response, status) {
-//         if (status === google.maps.DirectionsStatus.OK) {
-//             directionsDisplay.setDirections(response);
-
-//             service.getDistanceMatrix({
-//                 origins: [marker.getPosition()],
-//                 destinations: [newMarker.getPosition()],
-//                 travelMode: google.maps.TravelMode.DRIVING
-//             }, function (response, status) {
-//                 if (status !== google.maps.DistanceMatrixService.OK) {
-//                     // response
-//                     console.log(response);
-//                 }
-//             });
-//         } else {
-//             window.alert('Directions request failed due to ' + status);
-//         }
-//     });
-// }
+import superagent from "superagent";
 
 class Directions extends Component {
 
-    'user strick';
+    constructor() {
+        super()
+        // Setting state to store data from the API return
+        // REMEMBER THAT VENUES WILL UPDATE BASED ON THE APIs JSON parse!!!!!
+        this.state = {
+            venues: []
+        }
+    }
 
-    // call functions
-    // initMap()
-    // ZoomControl()
-    // GeolocationControl()
-    // });
+    // lifecycle method: triggering this function when the component mounts
+    // fires an API request to receive initial set of local hospital data
+    componentDidMount() {
+        console.log('componentDidMount');
 
-    // "https://maps.googleapis.com/maps/api/distancematrix/json?parameters"
 
-    // Google Directions API
-    // https://developers.google.com/maps/documentation/directions/intro?hl=en
+        const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.5074066,-81.60832649999999&radius=50&types=hospital&&key=AIzaSyCIONgJx9RS8JG4QFsjD5fi3PyLrPEOpuQ";
+
+        // chaining four functions onto each other
+        superagent
+            .get(url)
+            .query(null)
+            .set('Accept', 'text/json')
+            .end((error, response) => {
+
+                // DEPENDS ON LOGIC OF API
+                // const venues = response.body.response.venues;
+
+                // Supplied local hospital data within 25 mile radius from Google Places API
+                // console.log(JSON.stringify(venues));
+
+
+                // this.setState({
+                //     venues: venues
+                // })
+            })
+    }
 
     render() {
+
+        const location = {
+            // Thwing
+            lat: 41.5074066,
+            lng: -81.60832649999999
+        }
+
+        const markers = [
+            {
+                location: {
+                    // Thwing
+                    lat: 41.5074066,
+                    lng: -81.60832649999999
+                }
+            }
+        ];
+
         return (
+
             <div>
-                <h1>This is where the directions API goes</h1>
+
+                At least this is working!
+
+                <div style={{ width: 300, height: 600 }}>
+                    {/* The markers are the venues/hospitals where you need to go */}
+                    <Map center={location} markers={this.state.venues} />
+                </div>
+
+                <Places venues={this.state.venues} />
+
             </div>
+
         )
     }
-};
+}
 
 export default Directions;
