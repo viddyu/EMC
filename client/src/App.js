b55cfcc8ff5ea390a0da2b9ce3e2286275142a3a
David <viddyshep@gmail.com>
2018-07-16 21:40:05 -0400

    route fixes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar.js";
 import Records from "./pages/Records/Records.js";
 // import User from "./pages/User/User.js";
 import Form from "./components/Form/Form.js";
-import Chat from "./pages/chat/Chat.js";
+import Chat from "./pages/Chat/Chat.js";
 import Directions from "./pages/Directions/Directions.js";
 import LoginLogout from "./components/Login/Login.js"
 import Status from "./components/Login/Status/Status.js"
